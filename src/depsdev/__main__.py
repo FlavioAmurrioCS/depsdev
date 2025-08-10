ba25f68a4aad89390674e7e5b54839c01b6d334a
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2025-08-10 00:20:45 -0400

    Update dependencies and enhance CLI functionality with async support

--- src/depsdev/__main__.py
+++ src/depsdev/__main__.py
@@ -1,14 +1,72 @@
 from __future__ import annotations
 
+import asyncio
+import functools
+import logging
+import shutil
+from typing import TYPE_CHECKING
 
-def main(argv: list[str] | None = None) -> int:
-    import argparse
+import typer
 
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args(argv)
-    print(f"Arguments: {vars(args)=}")
-    return 0
+from depsdev import DepsDevClient
 
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from typing_extensions import ParamSpec
+    from typing_extensions import TypeVar
+
+    P = ParamSpec("P")
+    R = TypeVar("R")
+
+main = typer.Typer(
+    name="depsdev",
+    help="A CLI tool to interact with the Deps.Dev API.",
+    no_args_is_help=True,
+    context_settings={
+        "max_content_width": shutil.get_terminal_size().columns,
+    },
+)
+
+client = DepsDevClient()
+
+
+def to_sync() -> Callable[[Callable[P, R]], Callable[P, R]]:
+    """
+    Decorator to convert async methods to sync methods.
+    """
+
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @functools.wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            try:
+                from rich import print_json
+
+                print_json(data=asyncio.run(func(*args, **kwargs)))  # type: ignore[arg-type]
+            except:  # noqa: E722
+                raise SystemExit(1) from None
+
+            raise SystemExit(0)
+
+        return wrapper
+
+    return decorator
+
+
+main.command()(to_sync()(client.get_package))
+main.command()(to_sync()(client.get_version))
+main.command()(to_sync()(client.get_requirements))
+main.command()(to_sync()(client.get_dependencies))
+main.command()(to_sync()(client.get_project))
+main.command()(to_sync()(client.get_project_package_versions))
+main.command()(to_sync()(client.get_advisory))
+main.command()(to_sync()(client.query))
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format="[%(asctime)s] [%(levelname)-7s] [%(name)s] %(message)s",
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    main()
